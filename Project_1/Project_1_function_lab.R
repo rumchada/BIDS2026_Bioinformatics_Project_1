3ecb71b4dd5440b0a87c83f349960289177076fe
Geordan Bolden <gbolden101@gmail.com>
2026-05-26 14:56:38 -0400

    function update and expansion: enrichgo_unpacking visualizations

--- Project_1/Project_1_function_lab.R
+++ Project_1/Project_1_function_lab.R
@@ -516,6 +516,259 @@ heatmap_function <- function(initial_table, table_list) {
   return(heatmap_list)
 }
 
+ora_dotplot <- function(ora_results, gspval_cutoff, org_by, num_disp, graph_title, file_name){
+  if(org_by %in% 'Count'){
+    #inside if
+    dotplot <- ora_results%>%
+      arrange(desc(Count))%>%
+      head(num_disp)%>%
+      mutate(as.factor(Description))%>%
+      ggplot() +
+      aes(y = Count, x = fct_reorder(Description, Count)) +
+      scale_colour_gradient(low = "red", high = "blue") +
+      geom_point(aes(size = GeneRatio, color = pvalue)) +
+      labs(title = graph_title, x = "Enriched Term", y = "Gene Count") +
+      coord_flip() +
+      theme(text = element_text(face = "bold") , plot.title = element_text(hjust = 1))
+    
+  }
+  if(org_by %in% 'pvalue'){
+    #inside if
+    dotplot <- ora_results %>%
+      filter(pvalue < gspval_cutoff) %>%
+      head(num_disp) %>%
+      mutate(as.factor(Description)) %>%
+      mutate(pvalue = -1 * log(pvalue)) %>%
+      ggplot() +
+      aes(x = pvalue, y = fct_reorder(Description, pvalue))+
+      scale_colour_gradient(low = "red", high = "blue") +
+      geom_point(aes(size = GeneRatio, color = Count)) +
+      labs(title = graph_title, x = "-log10(P)", y = "Enriched Term") +
+      theme(text = element_text(face = "bold"), plot.title = element_text(hjust = 1))
+    
+    
+  }
+  return(list(dotplot, ggsave(file_name, device = "png", width = 8, height = 6, units = "in")))
+}
+
+
+ora_enrichgo <- function(filtered_results,
+                         direction = c("up", "down"),
+                         log2fc_thresh = 1,
+                         pval_thresh = 0.05,
+                         top_terms = 15,
+                         OrgDb = org.Hs.eg.db,
+                         keyType = "ENSEMBL",
+                         ont = "BP"){
+  
+  direction <- match.arg(direction)
+  
+  # store enrichment results
+  enrichgo_results <- vector("list", length(filtered_results))
+  names(enrichgo_results) <- names(filtered_results)
+  
+  # run enrichGO
+  for(i in seq_along(filtered_results)){
+    
+    if(direction == "up"){
+      
+      genes <- filtered_results[[i]]$geneid[
+        filtered_results[[i]]$log2foldchange > log2fc_thresh
+      ]
+      
+    } else if(direction == "down"){
+      
+      genes <- filtered_results[[i]]$geneid[
+        filtered_results[[i]]$log2foldchange < -log2fc_thresh
+      ]
+      
+    }
+    
+    enrichgo_results[[i]] <- clusterProfiler::enrichGO(
+      gene = genes,
+      OrgDb = OrgDb,
+      keyType = keyType,
+      ont = ont,
+      pAdjustMethod = "BH"
+    )
+  }
+  
+  # store visualization outputs
+  enrichgo_visuals <- vector("list", length(enrichgo_results))
+  names(enrichgo_visuals) <- names(enrichgo_results)
+  
+  # generate plots
+  for(i in seq_along(enrichgo_results)){
+    
+    ora_dotplot_count <- ora_dotplot(
+      enrichgo_results[[i]],
+      pval_thresh,
+      "Count",
+      top_terms,
+      glue::glue(
+        "ORA Results Ranked by Count: {names(enrichgo_results)[i]}"
+      ),
+      glue::glue(
+        "ORA Results Ranked by Count: {names(enrichgo_results)[i]}.png"
+      )
+    )
+    
+    ora_dotplot_pvalue <- ora_dotplot(
+      enrichgo_results[[i]],
+      pval_thresh,
+      "pvalue",
+      top_terms,
+      glue::glue(
+        "ORA Results Ranked by PValue: {names(enrichgo_results)[i]}"
+      ),
+      glue::glue(
+        "ORA Results Ranked by PValue: {names(enrichgo_results)[i]}.png"
+      )
+    )
+    
+    enrichgo_visuals[[i]] <- list(
+      ora_count_ranked = ora_dotplot_count,
+      ora_pvalue_ranked = ora_dotplot_pvalue
+    )
+  }
+  
+  return(list(
+    enrichgo_results = enrichgo_results,
+    enrichgo_visuals = enrichgo_visuals
+  ))
+}
+
+enrichgo_unpack_ver3 <- function(
+    # Pass the whole list so we can grab comparisons
+  initial_table_list = filtered_results,
+  key = ora_output_down$enrichgo_results,
+  pval_adj_threshold = 0.001,
+  
+  # Assume user does not want to convert geneids
+  convert = FALSE,
+  from_type = "ensembl",
+  to_type = "symbol",
+  ensembl_dataset = "hsapiens_gene_ensembl"){
+  require(tibble)
+  require(dplyr)
+  
+  ### helper functionf or converting geneidA to geneidB
+  ### user
+  gene_id_converter <- function(vector, from_type, to_type, ensembl_dataset){
+    require(biomaRt)
+    # geneid pulling for correct attribute
+    id_map <- c(
+      ensembl = "ensembl_gene_id",
+      entrez  = "entrezgene_id",
+      symbol  = "external_gene_name"
+    )
+    # p
+    from_attr <- id_map[[from_type]]
+    to_attr <-id_map[[to_type]]
+    
+    ensembl <- useMart(biomart = "ensembl", dataset = ensembl_dataset)
+    
+    return_df <- getBM( attributes = c(from_attr, to_attr), filters = from_attr, values = vector, mart = ensembl
+    )
+    return(return_df)
+    
+  }
+  
+  ###creates a helper function to unlist and strsplits the enriched Gene IDs per term
+  unlist_converter <- function(str) {
+    if (grepl("^ENSG|\\d", str)){
+      bruh <- as.character(unlist(strsplit(str, "/")))
+    }else{
+      bruh <- as.integer(unlist(strsplit(str, "/")))
+    }
+    return(bruh)
+  }
+  
+  
+  # FIX 2: Corrected the spelling typo here
+  unpacking_results <- list()
+  
+  # FIX 1: Loop over names(key) so 'comparison' is a valid character string identifier
+  for (comparison in names(key)) {
+    
+    # Safeguard check: If the comparison doesn't exist in the cluster results, skip
+    if (is.null(key[[comparison]])) next
+    
+    cluster <- tibble::as_tibble(key[[comparison]]@result) %>%
+      dplyr::mutate(Description = as.character(Description))
+    
+    if (!is.null(pval_adj_threshold)) {
+      significant_clusters <- cluster %>%
+        dplyr::filter(p.adjust < pval_adj_threshold)
+    } else {
+      significant_clusters <- cluster
+    }
+    
+    labeled_gene_lists <- list()
+    
+    # FIX 3: Pull the specific comparison dataframe from your filtered results list
+    current_initial_table <- initial_table_list[[comparison]] %>%
+      dplyr::mutate(geneid = as.character(geneid))
+    
+    # If there are no significant clusters, jump to saving the empty list or next comparison
+    if (nrow(significant_clusters) == 0) {
+      unpacking_results[[comparison]] <- labeled_gene_lists
+      next
+    }
+    
+    for (i in seq_len(nrow(significant_clusters))) {
+      
+      go_term <- significant_clusters$Description[i]
+      
+      # Extract genes for current GO term
+      genes <- unlist_converter(significant_clusters$geneID[i])
+      
+      pathway_tibble <- tibble::tibble(
+        gene_desc = rep(go_term, length(genes)), 
+        geneid = as.character(genes)
+      )
+      
+      if (convert == FALSE) {
+        pathway_genenames <- pathway_tibble
+      } else {
+        pathway_genenames <- gene_id_converter(
+          pathway_tibble$geneid,
+          from_type = from_type,
+          to_type = to_type,
+          ensembl_dataset = ensembl_dataset
+        )
+        
+        new_colname <- paste0(to_type, "_geneid")
+        
+        # FIX 4: Cleaner, bulletproof column renaming by position
+        colnames(pathway_genenames)[1] <- "geneid"
+        colnames(pathway_genenames)[2] <- new_colname
+      }
+      
+      # Join converted names
+      final_path_tibble <- dplyr::inner_join(pathway_tibble, pathway_genenames, by = "geneid") %>%
+        dplyr::mutate(geneid = as.character(geneid))
+      
+      # Join fold changes / statistics using our dynamically updated initial table
+      final_path_fc_tibble <- dplyr::inner_join(final_path_tibble, current_initial_table, by = "geneid")
+      
+      # Relocate converted gene column
+      if (convert == TRUE) {
+        fpfc_tibble <- final_path_fc_tibble %>% 
+          dplyr::relocate(dplyr::all_of(new_colname), .after = geneid)
+      } else {
+        fpfc_tibble <- final_path_fc_tibble
+      }
+      
+      # Store result under the GO Term name
+      labeled_gene_lists[[go_term]] <- fpfc_tibble
+    }
+    
+    # Store the list of terms into the master list under the Comparison name
+    unpacking_results[[comparison]] <- labeled_gene_lists
+  }
+  return(unpacking_results)
+}
 
 
 
